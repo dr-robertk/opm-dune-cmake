41c87b285231d897b4ae89be8df4e7e437063090
Andreas Lauser <and@poware.org>
2017-12-18 09:38:08 +0100

    make it possible to specify the vanilla cmake binary
    this can be done by setting the VANILLA_CMAKE environment variable
    before calling the wrapper script. it allows to nest cmake wrapper
    scripts or to install cmake in a location that is not in $PATH.

--- bin/cmake-wrapper.sh
+++ bin/cmake-wrapper.sh
@@ -1,13 +1,15 @@
 #!/bin/bash
 
 ORIG_FLAGS=( "$@" )
-REAL_CMAKE="$(which cmake)"
+if test -z "$VANILLA_CMAKE"; then
+    VANILLA_CMAKE="$(which cmake)"
+fi
 OPM_CMAKE_DIR="$(readlink -f $(dirname $(which $0))/..)"
 
 # check if we're called with the --build flag. in this case, just use plain cmake
 for FLAG in $ORIG_FLAGS; do
     if test "$FLAG" == "--build"; then
-        exec $REAL_CMAKE "${ORIG_FLAGS[@]}"
+        exec $VANILLA_CMAKE "${ORIG_FLAGS[@]}"
     fi
 done
 
@@ -87,7 +89,7 @@ EOF
             -DCMAKE_MODULE_PATH=*)
                 # ignore module path arguments
                 ;;
-            
+
             -*)
                 # copy all other flags
                 NEW_FLAGS+=("${FLAG}")
@@ -102,11 +104,11 @@ EOF
     done
 
     echo -n "running bare cmake:"
-    echo -n "$REAL_CMAKE "    
+    echo -n "$VANILLA_CMAKE "
     print_flags "${NEW_FLAGS[@]}"
     echo
 
-    exec $REAL_CMAKE "${NEW_FLAGS[@]}"
+    exec $VANILLA_CMAKE "${NEW_FLAGS[@]}"
     exit 0
 }
 
@@ -129,6 +131,6 @@ case "$MODULE_NAME" in
     *)
         echo "Unrecognized module '$MODULE_NAME' in directory '$SOURCE_DIR'. Using vanilla cmake."
 
-        exec "$REAL_CMAKE" "${ORIG_FLAGS[@]}"
+        exec "$VANILLA_CMAKE" "${ORIG_FLAGS[@]}"
         ;;
 esac
