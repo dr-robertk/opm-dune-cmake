15ea284f35402818f5a48276f0673ce8707a48af
Andreas Lauser <and@poware.org>
2018-02-07 10:00:33 +0100

    make opm-common optional for ewoms and opm-material

--- opm-common-buildfiles/cmake/modules/OpmCommonMacros.cmake
+++ opm-common-buildfiles/cmake/modules/OpmCommonMacros.cmake
@@ -3,31 +3,3 @@
 # This module's content is executed whenever a Dune module requires or
 # suggests opm-common!
 #
-
-option(ENABLE_OPENMP "Turn OpenMP on if it is available?" OFF)
-if(ENABLE_OPENMP)
-  find_package(OpenMP)
-
-  if(OPENMP_FOUND)
-    set(HAVE_OPENMP 1)
-    dune_register_package_flags(
-      COMPILE_OPTIONS "${OpenMP_CXX_FLAGS}"
-      LIBRARIES "${OpenMP_CXX_FLAGS}")
-  endif()
-endif()
-
-# Check if valgrind library is available or not.
-find_package(Valgrind)
-
-# export include flags for valgrind
-set(HAVE_VALGRIND "${VALGRIND_FOUND}")
-if(VALGRIND_FOUND)
-  dune_register_package_flags(
-    INCLUDE_DIRS "${VALGRIND_INCLUDE_DIR}")
-endif()
-
-# add a work-around for the missing HAVE_PTHREAD config.h
-# variable. the actual threads detection is done by dune-common.
-if (THREADS_HAVE_PTHREAD_ARG)
-  set(HAVE_PTHREAD 1)
-endif()
